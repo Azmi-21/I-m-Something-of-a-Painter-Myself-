72a03ea4b8a85735915dbd050c483be394dd9e13
Azmi Abidi <a_abidi@live.concordia.ca>
2025-10-29 13:22:01 -0400

    Initialize repository structure

---
+++ src/models/__init__.py
@@ -0,0 +1,8 @@
+"""
+Models module for GAN architectures.
+
+This module will contain implementations of various GAN models including:
+- Generator networks
+- Discriminator networks
+- Custom architectures (DCGAN, FastCUT, Latent-Diffusion, etc.)
+"""
---
+++ src/training/__init__.py
@@ -0,0 +1,9 @@
+"""
+Training module for GAN training loops and optimization.
+
+This module will contain:
+- Training loops for different GAN architectures
+- Loss functions and optimization strategies
+- Training utilities and callbacks
+- Checkpoint management
+"""
